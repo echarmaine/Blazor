85b55117c5ba3f057ff331b0fea0e276fa298ea6
echarmaine <110159036+echarmaine@users.noreply.github.com>
2022-08-01 18:18:28 +0800

    Add files via upload

---
+++ BlazorAppPizza/BlazorAppPizza/Data/PizzaMenu.cs
@@ -0,0 +1,32 @@
+﻿namespace BlazorAppPizza.Data
+{
+    public class PizzaMenu
+    {
+        public string ImageTitle { get; set; }
+
+        public string PizzaName { get; set; }
+
+        public string Toppings { get; set; }
+
+        public float PizzaPrice { get; set; }
+
+
+        public bool Cheese { get; set; }
+
+        public bool Pepperoni { get; set; }
+
+        public bool Mushrooms { get; set; }
+
+        public bool Tuna { get; set; }
+
+        public bool Pineapple { get; set; }
+
+        public bool Ham { get; set; }
+
+        public bool Beef { get; set; }
+
+        public int Quantity { get; set; }
+
+        public float FinalPrice { get; set; }
+    }
+}
---
+++ BlazorAppPizza/BlazorAppPizza/Data/PizzaMenuList.cs
@@ -0,0 +1,25 @@
+﻿namespace BlazorAppPizza.Data
+{
+    public class PizzaMenuList
+    {
+        public static List<PizzaMenu> PizzaList = new List<PizzaMenu>()
+        {
+            new PizzaMenu() {ImageTitle = "Bolognese", PizzaName = "Bolognese"},
+            new PizzaMenu() {ImageTitle = "Carbonara", PizzaName = "Carbonara"},
+            new PizzaMenu() {ImageTitle = "Hawaiian", PizzaName = "Hawaiian"},
+            new PizzaMenu() {ImageTitle = "Margerita", PizzaName = "Margerita"},
+            new PizzaMenu() {ImageTitle = "MeatFeast", PizzaName = "MeatFeast"},
+            new PizzaMenu() {ImageTitle = "Mushroom", PizzaName = "Mushroom"},
+            new PizzaMenu() {ImageTitle = "Pepperoni", PizzaName = "Pepperoni"},
+            new PizzaMenu() {ImageTitle = "Seafood", PizzaName = "Seafood"},
+            new PizzaMenu() {ImageTitle = "Vegetarian", PizzaName = "Vegetarian"},
+            new PizzaMenu() {ImageTitle = "Customized", PizzaName = "Customized"},
+        };
+
+
+        public static Task<List<PizzaMenu>> GetPizzaList()
+        {
+            return Task.FromResult(PizzaList);
+        }
+    }
+}
